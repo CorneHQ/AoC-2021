8ae091987673d5f58b3fbdf67f3a86e98594c8c4
Corné den Breejen <cornedev@outlook.com>
2021-12-01 09:30:44 +0100

    Added part two of day one solution

--- day1/main.py
+++ day1/main.py
@@ -9,6 +9,7 @@ def getPuzzleCode() -> List[str]:
 def convertStringsToInts(listStrings: List[str]) -> List[int]:
     return list(map(lambda x: int(x), listStrings))
 
+#Part 1
 def countIncreases(listInts: List[int]) -> int:
     count = 0
     for index, element in enumerate(listInts[:-1]):
@@ -16,7 +17,22 @@ def countIncreases(listInts: List[int]) -> int:
             count += 1
     return count
 
+# Part 2
+def countIncreaseWindows(listInts: List[int]) -> int:
+    count = 0
+    for index, element in enumerate(listInts[:-3]):
+        firstWindow = element + listInts[index + 1] + listInts[index + 2]
+        secondWindow = listInts[index + 1] + listInts[index + 2] + listInts[index + 3]
+        if secondWindow > firstWindow:
+            count += 1
+    return count
+
 if __name__ == "__main__":
     puzzleCodeStrings = getPuzzleCode()
     puzzleCode = convertStringsToInts(puzzleCodeStrings)
+
+    #Part 1
     print(countIncreases(puzzleCode))
+
+    # Part 2
+    print(countIncreaseWindows(puzzleCode))
