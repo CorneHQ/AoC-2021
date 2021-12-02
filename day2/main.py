7e54642f6387944754db77b40a27bc2f2f2b24b9
Corné den Breejen <cornedev@outlook.com>
2021-12-02 09:01:53 +0100

    Added part two solution for day two

--- day2/main.py
+++ day2/main.py
@@ -25,9 +25,29 @@ def calculateDepth(list: List[Dict[str, int]]) -> int:
 def calculateHorizontal(list: List[Dict[str, int]]) -> int:
     return sum([element["amount"] for element in list if element["direction"] == "forward"])
 
+def calculatePreciseRoute(list: List[Dict[str, int]]) -> int:
+    depth = 0
+    horizontal = 0
+    aim = 0
+    for element in list:
+        if element["direction"] == "forward":
+            horizontal += element["amount"]
+            depth += aim * element["amount"]
+        elif element["direction"] == "down":
+            aim += element["amount"]
+        elif element["direction"] == "up":
+            aim -= element["amount"]
+    return horizontal * depth
+
 if __name__ == '__main__':
     puzzleCodeString = getPuzzleCode()
     sortedPuzzleCode = sortPuzzleCode(puzzleCodeString)
+
+    # Part 1
     depth = calculateDepth(sortedPuzzleCode)
     horizontal = calculateHorizontal(sortedPuzzleCode)
     print(horizontal * depth)
+
+    # Part 2
+    preciseRoute = calculatePreciseRoute(sortedPuzzleCode)
+    print(preciseRoute)
